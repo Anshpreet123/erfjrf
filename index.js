408225b6e361615f3ff9fb1beddf7befc17c26fe
Anshpreet22 <210100018@iitb.ac.in>
2024-08-30 01:01:30 +0530

    kindof

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const app = express();
-
+const path = require('path');
 const PORT = 3000;
 
 app.use(express.json());
 app.get('/' , function (req , res){
+    
     res.json({
         "mess ":" successfully fucked up"
     })
 })
+app.get('/page', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
 
 app.listen(PORT , ()=>{
     console.log("App is listening on port : " , PORT);
